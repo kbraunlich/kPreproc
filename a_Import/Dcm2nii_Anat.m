5d323049b47ff03dde86c191b7ec7fa2ebbec283
kbraunlich <kbraunlich@gmail.com>
2015-03-12 23:12:27 -0600

    Update Dcm2nii_Anat.m

--- a_Import/Dcm2nii_Anat.m
+++ a_Import/Dcm2nii_Anat.m
@@ -8,7 +8,7 @@ clc
 
 %% ====== SETTINGS ============================================
 subs=[4];
-mri_fldr='/Volumes/Sarapiqui/valueAcmltr/mriData';
+mriFldr='/Volumes/Ernie/dev/nipype/extrapTutorialData/mriData';
 
 frmt= 'nii'; % 'img' or 'nii'
 %% -------------------------------------------------------------
@@ -19,50 +19,24 @@ for s=1:numel(subs)
     sub=subs(s)
     
     
-    inFldr=([mri_fldr '/s' sprintf('%3.3d',sub) '/raw']);
-    l=dir([inFldr '/*']);
-   
-    l = l(arrayfun(@(x) x.name(1), l) ~= '.');
-    inFldr=[inFldr '/' l.name];
+    inFldr=filenames([mriFldr '/s' sprintf('%3.3d',sub) '/raw/*mpr_sag*'],'char');
     
-    l2=dir([inFldr '/*mpr_sag*']);
-    inFldr=[inFldr  '/' l2.name]
-    
-    cd([inFldr]);
     %       cd([fold  '/run' num2str(run)]);
-    files = spm_select('list', pwd, '\.dcm');
-    
+    files=filenames([inFldr '/*.dcm'],'char');
     hdr = spm_dicom_headers(files);
-    out_fldr=[mri_fldr '/s' sprintf('%3.3d',sub) '/anat'];
+    out_fldr=[mriFldr '/s' sprintf('%3.3d',sub) '/anat'];
     if ~exist(out_fldr,'dir');mkdir(out_fldr);end
-    
+    cd(inFldr)
     spm_dicom_convert(hdr,'all','flat',frmt); % (hdr,opts,root_dir,format)
-    newFiles=dir('*.nii');
+    newFiles=filenames([inFldr '/*.nii'],'char');
     % move files to new dir
-    nFiles=length(newFiles);
+    nFiles=length(newFiles(:,1));
     for i=1:nFiles
-        movefile([inFldr '/' newFiles(i).name],[out_fldr '/' newFiles(i).name])
+        [p,old_nii_name,e]=fileparts(newFiles(i,:));
+        movefile( newFiles(i,:),[out_fldr '/s' sprintf('%3.3d',sub) '_' old_nii_name(end-6:end-3) '.nii'])
     end
     
     
-    % rename files
-    % =================================
-    cd(out_fldr)
-    % imported functional files will have a prefix 'f'
-    old_nii_names = dir('*.nii');
-    for n = 1:length(old_nii_names)
-        % need to count file name characters to determine the right
-        % range
-        % new file names will the the form of 'subjID-runx-0001.nii' PROB
-        % CHANGE TO "name(26:28)"
-        new_nii_name=['s' sprintf('%3.3d',sub) '_' old_nii_names(n).name(end-9:end-7)];
-        
-        new_nii_name = [new_nii_name '.nii'];
-        %move niis
-        movefile(old_nii_names(n).name,new_nii_name);
-        
-    end
-    % ---------------------------------
     
     disp(['finished renaming ' num2str(nFiles)  ' files']);
     disp(' ');
